76cd98a816bea399a7f86618c67a73d65e09e225
pete3n <pete3n@protonmail.com>
2024-01-29 17:14:38 -0500

    23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixvim.url = "github:nix-community/nixvim/nixos-23.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
