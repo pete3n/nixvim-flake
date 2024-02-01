773955f1dcc883c2d07ebfed333e61c9de7fb23c
Pete <pete3n@protonmail.com>
2024-02-01 08:35:25 -0500

    Test flake

--- flake.nix
+++ flake.nix
@@ -10,21 +10,8 @@
   outputs = { self, nixpkgs, nixvim, flake-parts, ... } @ inputs: let
     config = import ./config; # import the module directly
 
-    # Define the custom Neovim wrapper
-    customNeovimWrapper = { pkgs, ... }: let
-      neovimDeps = with pkgs; [
-        alejandra asmfmt astyle black cmake-format gofumpt golines gotools isort 
-        nodePackages.prettier prettierd shfmt stylua commitlint eslint_d hadolint 
-        html-tidy luajitPackages.luacheck nodePackages.jsonlint pylint ruff 
-        shellcheck vale yamllint tmux-sessionizer asm-lsp cargo clang-tools go rustc
-      ];
-    depsPath = builtins.concatStringsSep ":" (map (dep: "${dep}/bin") neovimDeps);
-    in pkgs.writeShellScriptBin "neovim-with-deps" ''
-        export PATH=${depsPath}:"$PATH"
-        exec ${pkgs.neovim}/bin/nvim "$@"
-    '';
-    in
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  in
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -34,16 +21,14 @@
 
       perSystem = { pkgs, system, ... }: let
         nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
-
-        # Use the custom Neovim wrapper
-        nvim = nixvim'.makeNixvimWithModule {
+        
+        # Use nixvim.legacyPackages.${system} directly
+        nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
           inherit pkgs;
           module = config;
         };
       in {
         checks = {
-          # Run `nix flake check .` to verify that your config is not broken
           default = nixvimLib.check.mkTestDerivationFromNvim {
             inherit nvim;
             name = "A nixvim configuration";
@@ -51,8 +36,7 @@
         };
 
         packages = {
-          # Provide the custom Neovim wrapper as a package
-          default = customNeovimWrapper { pkgs = pkgs; };
+          default = nvim;
         };
       };
     };
