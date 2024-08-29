64c68f86af777c85e56e5994d3b3792ba591825b
pete3n <pete3n@protonmail.com>
2024-08-29 11:23:45 -0400

    fix: refactor for new options option

--- config/telescope.nix
+++ config/telescope.nix
@@ -4,39 +4,57 @@
     keymaps = {
       "<leader>?" = {
         action = "oldfiles";
-        desc = "[?] Find recently opened files";
+        options = {
+          desc = "[?] Find recently opened files";
+        };
       };
       "<leader><space>" = {
         action = "buffers";
-        desc = "[ ] Find existing buffers";
+        options = {
+          desc = "[ ] Find existing buffers";
+        };
       };
       "<leader>/" = {
         action = "current_buffer_fuzzy_find";
-        desc = "[/] Fuzzily search in current buffer]";
+        options = {
+          desc = "[/] Fuzzily search in current buffer]";
+        };
       };
       "<leader>sf" = {
         action = "find_files";
-        desc = "[s]earch [f]iles";
+        options = {
+          desc = "[s]earch [f]iles";
+        };
       };
       "<leader>sh" = {
         action = "help_tags";
-        desc = "[s]earch [h]elp";
+        options = {
+          desc = "[s]earch [h]elp";
+        };
       };
       "<leader>sw" = {
         action = "grep_string";
-        desc = "[s]earch current [w]ord";
+        options = {
+          desc = "[s]earch current [w]ord";
+        };
       };
       "<leader>sg" = {
         action = "live_grep";
-        desc = "[s]earch by [g]rep";
+        options = {
+          desc = "[s]earch by [g]rep";
+        };
       };
       "<leader>sd" = {
         action = "diagnostics";
-        desc = "[s]earch [d]iagnotics";
+        options = {
+          desc = "[s]earch [d]iagnotics";
+        };
       };
       "<leader>sk" = {
         action = "keymaps";
-        desc = "[s]earch [k]eymaps";
+        options = {
+          desc = "[s]earch [k]eymaps";
+        };
       };
     };
   };
