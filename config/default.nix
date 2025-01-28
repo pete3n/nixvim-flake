bd58b7a7760fbe7c26be7332b527b283a304e2c9
pete3n <pete3n@protonmail.com>
2025-01-28 15:55:43 -0500

    feat: global options

--- config/default.nix
+++ config/default.nix
@@ -12,6 +12,29 @@
     stylua
   ];
 
+  globals = {
+    mapleader = " ";
+  };
+
+  opts = {
+    number = true;
+    colorcolumn = "80";
+    relativenumber = true;
+    shiftwidth = 2;
+    tabstop = 2;
+    wrap = false;
+    swapfile = false; # Undotree
+    backup = false; # Undotree
+    undofile = true;
+    hlsearch = false;
+    incsearch = true;
+    termguicolors = true;
+    scrolloff = 8;
+    signcolumn = "yes";
+    updatetime = 50;
+    foldlevelstart = 99;
+  };
+
   colorschemes.tokyonight = {
     enable = true;
     settings = {
@@ -56,11 +79,5 @@
         lua_ls.enable = true;
       };
     };
-  };
-
-  # Basic Neovim options
-  opts = {
-    number = true;
-    relativenumber = true;
   };
 }
