0042afe9ced6be04c6547fd350541c67a6622d2f
pete3n <pete3n@protonmail.com>
2025-01-28 18:01:17 -0500

    fix: enable nvim-cmp noice integration feat: disable CurrentScope highlights

--- config/parsing.nix
+++ config/parsing.nix
@@ -217,7 +217,7 @@
 
     treesitter-refactor = {
       enable = true;
-      highlightCurrentScope.enable = true;
+      highlightCurrentScope.enable = false;
       highlightDefinitions.enable = true;
       navigation = {
         enable = true;
--- config/styling.nix
+++ config/styling.nix
@@ -79,7 +79,7 @@
           					override = {
           						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           						["vim.lsp.util.stylize_markdown"] = true,
-          						-- TODO: ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
           					},
           				},
           			})
