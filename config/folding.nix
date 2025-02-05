2084bd4527941b158a8d548fbbf25446ff61888d
pete3n <pete3n@protonmail.com>
2025-02-05 17:23:13 -0500

    chore: folding refactor

--- config/folding.nix
+++ config/folding.nix
@@ -1,26 +1,44 @@
+# All configuration related to folding code blocks
 { ... }:
 {
   plugins.nvim-ufo = {
     enable = true;
     settings = {
-      provider_selector =
-        # lua
+      provider_selector = # lua
         ''
-          function(bufnr, filetype, buftype)
-           			return { 'lsp', 'indent' }
-           		end
+					function(bufnr, filetype, buftype)
+						return { 'lsp', 'indent' }
+					end
         '';
     };
-    luaConfig.pre =
+    luaConfig.post =
       # lua
       ''
-        -- Configuration for UFO folds
-        vim.keymap.set('n', 'zj', function()
-        	local winid = require('ufo').peekFoldedLinesUnderCursor()
-        	if not winid then
-        		vim.lsp.buf.hover()
-        	end
-        end, { desc = "Pee[k] fold" })
+				local peek_fold = function()
+					local winid = require('ufo').peekFoldedLinesUnderCursor()
+					if not winid then
+						vim.lsp.buf.hover()
+					end
+				end
+
+				if pcall(require, "which-key") then
+					local wk = require ("which-key")
+					wk.add({
+						{ "z", group = "folding", icon = "󰅪"},
+						{ "<leader>z", group = "folding", proxy = "z", icon = "󰅪", };
+					})
+				end
       '';
   };
+
+  keymaps = [
+    {
+      key = "zj";
+      mode = "n";
+      action = "<cmd>lua require('ufo').peekFoldedLinesUnderCursor()<CR>";
+      options = {
+        desc = "Peek folded lines";
+      };
+    }
+  ];
 }
