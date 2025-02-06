02c0cd43b75f35961b4bd3e910ef7a50efc1f1a2
pete3n <pete3n@protonmail.com>
2025-02-06 17:12:59 -0500

    feat: conditional kemappings

--- config/editing.nix
+++ config/editing.nix
@@ -1,12 +1,11 @@
 # All additional configuration related to editing additional files
 # (hex, git, etc.)
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 {
   extraPackages = with pkgs; [
     git
     xxd
   ];
-
 
   plugins = {
     hex.enable = true;
@@ -19,43 +18,48 @@
     };
     gitsigns.enable = true;
     neogit.enable = true;
-
+		vim-dadbod.enable = true;
+		vim-dadbod-ui.enable = config.plugins.vim-dadbod.enable; # Depedency
   };
 
-
-  keymaps = [
-    {
-      key = "<leader>u";
-      mode = "n";
-      action = "<cmd>UndotreeToggle<CR>";
-      options = {
-        desc = "undotree toggle";
-      };
-    }
-    {
-      key = "<leader>gf";
-      mode = "n";
-      action = "<cmd>Git<CR>";
-      options = {
-        desc = "git fugitive";
-      };
-    }
-    {
-      key = "<leader>gn";
-      mode = "n";
-      action = "<cmd>Neogit<CR>";
-      options = {
-        desc = "neogit";
-      };
-    }
-    {
-      key = "<leader>ga";
-      mode = "n";
-      action = "<cmd>AdvancedGitSearch<CR>";
-      options = {
-        desc = "advanced search";
-      };
-    }
+  keymaps = lib.concatLists [
+		(if config.plugins.undotree.enable then [
+			{
+				key = "<leader>u";
+				mode = "n";
+				action = "<cmd>UndotreeToggle<CR>";
+				options = {
+					desc = "undotree toggle";
+				};
+			}]	else [])
+		(if config.plugins.fugitive.enable then [
+			{
+				key = "<leader>gf";
+				mode = "n";
+				action = "<cmd>Git<CR>";
+				options = {
+					desc = "git fugitive";
+				};
+			}] else [])
+		(if config.plugins.neogit.enable then [
+			{
+				key = "<leader>gn";
+				mode = "n";
+				action = "<cmd>Neogit<CR>";
+				options = {
+					desc = "neogit";
+				};
+			}] else [])
+		( if builtins.elem "advanced_git_search" config.plugins.telescope.enabledExtensions then [
+			{
+				key = "<leader>ga";
+				mode = "n";
+				action = "<cmd>AdvancedGitSearch<CR>";
+				options = {
+					desc = "advanced search";
+				};
+			}] else [])
+		( if config.plugins.telescope.enable then [
     {
       key = "<leader>gs";
       mode = "n";
@@ -71,20 +75,11 @@
       options = {
         desc = "git branch";
       };
-    }
+    }]  else [])
   ];
 
   extraConfigLuaPost = # lua
     ''
-			-- Remove overlapping keymap from fugitive if it exists
-			local function rm_fugitive_keymap_overlap()
-				if vim.fn.mapcheck("y<C-G>", "n") ~= "" then
-					vim.keymap.del("n", "y<C-G>")
-				end
-			end
-
-			vim.schedule(rm_fugitive_keymap_overlap)
-
 			if pcall(require, "which-key") then
 				local wk = require("which-key")
 				
@@ -97,5 +92,16 @@
 					{ "<leader>ga", icon = " ", desc = "advanced search", },
 				})
 			end
-    '';
+    '' 
+		# Only delete fugitive overlap keys if it is enabled 
+		+ (if config.plugins.fugitive.enable then # lua
+		''
+			local function rm_fugitive_keymap_overlap()
+				if vim.fn.mapcheck("y<C-G>", "n") ~= "" then
+					vim.keymap.del("n", "y<C-G>")
+				end
+			end
+
+			vim.schedule(rm_fugitive_keymap_overlap)
+		'' else "");
 }
