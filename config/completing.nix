53b719e5ea5269de57adf5aaa97077ef3dcf116e
pete3n <pete3n@protonmail.com>
2025-02-09 09:01:55 -0500

    feat: search cmdline completion

--- config/completing.nix
+++ config/completing.nix
@@ -50,6 +50,13 @@
 					end
 
 					-- Extra options for cmp-cmdline setup
+					cmp.setup.cmdline('/', {
+						mapping = cmp.mapping.preset.cmdline(),
+							sources = {
+								{ name = 'buffer' }
+							}
+					})
+
 					cmp.setup.cmdline(":", {
 						mapping = cmp.mapping.preset.cmdline(),
 						sources = cmp.config.sources({
